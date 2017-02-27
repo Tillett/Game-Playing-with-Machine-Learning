7601bdee148a776dd7454b8908e8fbe1fb7b869a
Austin Auger <augera09@students.rowan.edu>
2017-02-26 19:16:55 -0500

    some updates

--- Super Mario Bros.lua
+++ Super Mario Bros.lua
@@ -2,6 +2,8 @@ require "table_utils"
 require "other_utils"
 
 math.randomseed(os.time())
+ss = savestate.create();
+savestate.save(ss);
 local PLAYER_DIRECTION_ADDR = 0x003
 local PL_LEFT               = 0
 local PL_RIGHT              = 1
@@ -12,7 +14,7 @@ local PLAYER_DOWN_HOLE      = 3
 local PLAYER_DYING_STATE    = 0x0B
 local PLAYER_DEAD_STATE     = 0x06
 local FRAME_MAX_PER_CONTROL = 20
-local xval                  = 0
+local TXT_INCR              = 9
 --local table = {}
 
 local candidate = { fitness = 0, 
@@ -29,12 +31,14 @@ local candidates = {  } -- new list of candidates
 
 local testCand = candidate:new();
 
-for i = 1, 100 do 
+for i = 1, 100 do
+    local lrv = random_bool()
+
     testCand.inputs[i] = { 
         up      = random_bool(),
         down    = random_bool(),
-        left    = random_bool(),
-        right   = random_bool(),
+        left    = lrv,
+        right   = not lrv,
         A       = random_bool(),
         B       = random_bool(),
         start   = false,
@@ -43,26 +47,37 @@ for i = 1, 100 do
 end
 
 while true do
+    savestate.load(ss);
+    local lframe = 0;
+    local xval = 0;
     local cnt = 0;
     local j = 1;
     local max = FRAME_MAX_PER_CONTROL*table.getn(testCand.inputs);
 
     for i = 1, max do
-        --joypad.set(1, testCand.inputs[j]);
+        joypad.set(1, testCand.inputs[j]);
 
         local onscreen_px = memory.readbyte(ONSCREEN_PX_ADDR);
         if onscreen_px ~= lframe then
             xval = xval + onscreen_px;
         end
-        gui.text(0, 15, xval);
-        local pstate = memory.readbyte(PLAYER_STATE_ADDR)
-        local fstate = memory.readbyte(PLAYER_VIEWPORT_ADDR);
+
+        gui.text(0, TXT_INCR * 2, "Best Horiz: "..xval);
+        
+        local pstate = memory.readbyte(PLAYER_STATE_ADDR);
+        local fstate = memory.readbyte(PLAYER_VIEWPORT_ADDR);   
         if pstate == PLAYER_DYING_STATE or fstate >= PLAYER_DOWN_HOLE then
-            gui.text(0, 33, "DYING!");
+            gui.text(0, TXT_INCR * 3, "DYING");
+            break
+        else
+            gui.text(0, TXT_INCR * 3, "ALIVE");
         end
+        
         lframe = memory.readbyte(ONSCREEN_PX_ADDR);
-        gui.text(0, 50, table.tostring(joypad.get(1)));
-        emu.frameadvance();
+        
+        tbl = joypad.get(1);
+        gui.text(0, TXT_INCR * 4, "Input: "..ctrl_tbl_btis(tbl));
+        gui.text(0, TXT_INCR * 5, "Curr Chromosome: "..j);
 
         cnt = cnt + 1;
         if cnt == FRAME_MAX_PER_CONTROL then
@@ -70,6 +85,7 @@ while true do
             j = j + 1
         end
 
-        gui.text(0, 67, j);
+        emu.frameadvance();
+        gui.text(0, TXT_INCR * 5, j);
     end
 end
--- other_utils.lua
+++ other_utils.lua
@@ -1,3 +1,21 @@
 function random_bool()
 	return math.random(1, 10) > 5;
 end
+
+function bti(bl)
+    return (bl and 1) or 0
+end
+
+function btis(bl)
+    return (bl and "1") or "0"
+end
+
+function ctrl_tbl_btis(tbl)
+    return (
+        btis( tbl.up    )..
+        btis( tbl.down  )..
+        btis( tbl.left  )..
+        btis( tbl.right )..
+        btis( tbl.A     )..
+        btis( tbl.B     ))
+end
